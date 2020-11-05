e7f9eee53251f20540ae694a2b4fdd99502cb9cd
MaHST0621 <1186217640@qq.com>
2020-11-05 21:36:20 +0800

    换个框架 我人没了。。。

--- IR作业/斯坦福-信息检索.py
+++ IR作业/斯坦福-信息检索.py
@@ -1,14 +1,16 @@
 # You can add additional imports here
 import sys
+import sys
 import pickle as pkl
 import array
 import os
 import timeit
 import contextlib
-i = 0
-
-##import urllib.request
-##import zipfile
+import nltk
+from math import log
+import heapq
+import urllib.request
+import zipfile
 
 # data_url = 'http://web.stanford.edu/class/cs276/pa/pa1-data.zip'
 # data_dir = 'pa1-data'
@@ -18,71 +20,56 @@ i = 0
 # zip_ref.close()
 
 
-# try:
-#     os.mkdir('output_dir')
-# except FileExistsError:
-#     pass
-# try:
-#     os.mkdir('tmp')
-# except FileExistsError:
-#     pass
-# try:
-#     os.mkdir('toy_output_dir')
-# except FileExistsError:
-#     pass
-#
-# sorted(os.listdir('pa1-data'))
-#
-# sorted(os.listdir('pa1-data/0'))[:10]
-#
-# with open('pa1-data/0/3dradiology.stanford.edu_', 'r') as f:
-#     print(f.read())
-#
-# toy_dir = 'toy-data'
+try:
+    os.mkdir('output_dir')
+except FileExistsError:
+    pass
+try:
+    os.mkdir('tmp')
+except FileExistsError:
+    pass
+try:
+    os.mkdir('toy_output_dir')
+except FileExistsError:
+    pass
+
+sorted(os.listdir('pa1-data'))
+
+sorted(os.listdir('pa1-data/0'))[:10]
+
+with open('pa1-data/0/3dradiology.stanford.edu_', 'r') as f:
+    print(f.read())
+
+toy_dir = 'toy-data'
 
 
 class IdMap:
-    """Helper class to store a mapping from strings to ids."""
 
     def __init__(self):
         self.str_to_id = {}
         self.id_to_str = []
-        global id_nedd
-
 
     def __len__(self):
-        """Return number of terms stored in the IdMap"""
         return len(self.id_to_str)
 
     def _get_str(self, i):
-        """Returns the string corresponding to a given id (`i`)."""
-        ### Begin your code
         return self.id_to_str[i]
-        ### End your code
 
     def _get_id(self, s):
-        """Returns the id corresponding to a string (`s`).
-        If `s` is not in the IdMap yet, then assigns a new id and returns the new id.
-        """
 
-        ### Begin your code
         if s not in self.str_to_id.keys():
-            Id_need = len(self.id_to_str)
+            id_need = len(self.id_to_str)
             self.id_to_str.append(s)
-            return self.str_to_id.setdefault(s,Id_need)
+            return self.str_to_id.setdefault(s, id_need)
         return self.str_to_id[s]
-        ### End your code
 
     def __getitem__(self, key):
-        """If `key` is a integer, use _get_str;
-           If `key` is a string, use _get_id;"""
         if type(key) is int:
             return self._get_str(key)
         elif type(key) is str:
             return self._get_id(key)
         else:
             raise TypeError
-
 
 
 class UncompressedPostings:
@@ -94,19 +81,14 @@ class UncompressedPostings:
 
     @staticmethod
     def decode(encoded_postings_list):
-
         decoded_postings_list = array.array('L')
         decoded_postings_list.frombytes(encoded_postings_list)
         return decoded_postings_list.tolist()
 
 
-
-
 class InvertedIndex:
 
-
     def __init__(self, index_name, postings_encoding=None, directory=''):
-
 
         self.index_file_path = os.path.join(directory, index_name + '.index')
         self.metadata_file_path = os.path.join(directory, index_name + '.dict')
@@ -119,13 +101,10 @@ class InvertedIndex:
 
         self.postings_dict = {}
         self.terms = []
-
-
     def __enter__(self):
 
         self.index_file = open(self.index_file_path, 'rb+')
 
-        # Load the postings dict and terms from the metadata file
         with open(self.metadata_file_path, 'rb') as f:
             self.postings_dict, self.terms = pkl.load(f)
             self.term_iter = self.terms.__iter__()
@@ -133,19 +112,11 @@ class InvertedIndex:
         return self
 
     def __exit__(self, exception_type, exception_value, traceback):
-        """Closes the index_file and saves metadata upon exiting the context"""
-        # Close the index file
         self.index_file.close()
-
-        # Write the postings dict and terms to the metadata file
         with open(self.metadata_file_path, 'wb') as f:
             pkl.dump([self.postings_dict, self.terms], f)
 
-
-# Do not make any changes here, they will be overwritten while grading
 class BSBIIndex:
-
-
     def __init__(self, data_dir, output_dir, index_name="BSBI",
                  postings_encoding=None):
         self.term_id_map = IdMap()
@@ -154,28 +125,21 @@ class BSBIIndex:
         self.output_dir = output_dir
         self.index_name = index_name
         self.postings_encoding = postings_encoding
-
-        # Stores names of intermediate indices
         self.intermediate_indices = []
 
     def save(self):
-        """Dumps doc_id_map and term_id_map into output directory"""
-
         with open(os.path.join(self.output_dir, 'terms.dict'), 'wb') as f:
             pkl.dump(self.term_id_map, f)
         with open(os.path.join(self.output_dir, 'docs.dict'), 'wb') as f:
             pkl.dump(self.doc_id_map, f)
 
     def load(self):
-        """Loads doc_id_map and term_id_map from output directory"""
-
         with open(os.path.join(self.output_dir, 'terms.dict'), 'rb') as f:
             self.term_id_map = pkl.load(f)
         with open(os.path.join(self.output_dir, 'docs.dict'), 'rb') as f:
             self.doc_id_map = pkl.load(f)
 
     def index(self):
-
         for block_dir_relative in sorted(next(os.walk(self.data_dir))[1]):
             td_pairs = self.parse_block(block_dir_relative)
             index_id = 'index_' + block_dir_relative
@@ -197,46 +161,18 @@ class BSBIIndex:
                                           self.postings_encoding))
                     for index_id in self.intermediate_indices]
                 self.merge(indices, merged_index)
-
-
 class BSBIIndex(BSBIIndex):
     def parse_block(self, block_dir_relative):
-
-        ### Begin your code
         word_doc = []
         doc_path = self.data_dir + '/' + block_dir_relative + '/'
         for i in os.listdir(doc_path):
             doc_id = self.doc_id_map[doc_path + i]
             f = open(doc_path + i)
-        #     words = f.read()
-        #
-        #     for word in words:
-        #         print(word,doc_id)
-        #         word_doc.append(self.term_id_map[word],doc_id)
-        # return word_doc
-            for line in f.readlines():
-                for word in line.strip().split(' '):
-                    print(word,i)
-                    word_doc.append((self.term_id_map[word],doc_id))
+        for line in f.readlines():
+            for terms in line.strip().split(' '):
+                print(terms, i)
+                word_doc.append((self.term_id_map[terms], doc_id))
         return word_doc
-        ### End your code
-
-
-
-
-with open('toy-data/0/fine.txt', 'r') as f:
-    print(f.read())
-with open('toy-data/0/hello.txt', 'r') as f:
-    print(f.read())
-
-
-BSBI_instance = BSBIIndex(data_dir=toy_dir, output_dir = 'tmp/', index_name = 'toy')
-BSBI_instance.parse_block('0')
-
-
-### Begin your code
-
-### End your code
 
 
 class InvertedIndexWriter(InvertedIndex):
@@ -247,75 +183,47 @@ class InvertedIndexWriter(InvertedIndex):
         return self
 
     def append(self, term, postings_list):
-
-        ### Begin your code
-
-        ### End your code
-
-
-
-
-with InvertedIndexWriter('test', directory='tmp/') as index:
-    index.append(1, [2, 3, 4])
-    index.append(2, [3, 4, 5])
-    index.index_file.seek(0)
-    assert index.terms == [1,2], "terms sequence incorrect"
-    assert index.postings_dict == {1: (0, 3, len(UncompressedPostings.encode([2,3,4]))),
-                                   2: (len(UncompressedPostings.encode([2,3,4])), 3,
-                                       len(UncompressedPostings.encode([3,4,5])))}, "postings_dict incorrect"
-    assert UncompressedPostings.decode(index.index_file.read()) == [2, 3, 4, 3, 4, 5], "postings on disk incorrect"
-
-
+        encoded_postings_list = self.postings_encoding.encode(postings_list)
+        self.terms.append(term)
+        self.postings_dict[term] = (self.index_file.tell(), len(postings_list), len(encoded_postings_list))
+        self.index_file.write(encoded_postings_list)
 class BSBIIndex(BSBIIndex):
     def invert_write(self, td_pairs, index):
-
-        ### Begin your code
-
-        ### End your code
-
-
-
-
-
-### Begin your code
-
-### End your code
+        term_id = -1
+        postings_list = []
+        for pair in sorted(set(td_pairs)):
+            if pair[0] != term_id:
+                if term_id != -1:
+                    index.append(term_id, postings_list)
+                term_id = pair[0]
+                postings_list = []
+            postings_list.append(pair[1])
+        index.append(term_id, postings_list)
 
 
 class InvertedIndexIterator(InvertedIndex):
-    """"""
-
     def __enter__(self):
-        """Adds an initialization_hook to the __enter__ function of super class
-        """
         super().__enter__()
         self._initialization_hook()
         return self
 
     def _initialization_hook(self):
-        """Use this function to initialize the iterator
-        """
-        ### Begin your code
-
-        ### End your code
-
+        self.term_idx = 0
     def __iter__(self):
         return self
 
     def __next__(self):
-
-        ### Begin your code
-
-        ### End your code
-
+        if self.term_idx == len(self.terms):
+            raise StopIteration
+        term = self.terms[self.term_idx]
+        self.term_idx += 1
+        pos, length, size = self.postings_dict[term]
+        self.index_file.seek(pos)
+        return (term, self.postings_encoding.decode(self.index_file.read(size)))
     def delete_from_disk(self):
-        """Marks the index for deletion upon exit. Useful for temporary indices
-        """
         self.delete_upon_exit = True
 
     def __exit__(self, exception_type, exception_value, traceback):
-        """Delete the index file upon exiting the context along with the
-        functions of the super class __exit__ function"""
         self.index_file.close()
         if hasattr(self, 'delete_upon_exit') and self.delete_upon_exit:
             os.remove(self.index_file_path)
@@ -324,58 +232,18 @@ class InvertedIndexIterator(InvertedIndex):
             with open(self.metadata_file_path, 'wb') as f:
                 pkl.dump([self.postings_dict, self.terms], f)
 
-
-
-
-
-
-### Begin your code
-
-### End your code
-
-
-
-import heapq
-animal_lifespans = [('Giraffe', 28),
-                   ('Rhinoceros', 40),
-                   ('Indian Elephant', 70),
-                   ('Golden Eagle', 80),
-                   ('Box turtle', 123)]
-
-tree_lifespans = [('Gray Birch', 50),
-                  ('Black Willow', 70),
-                  ('Basswood', 100),
-                  ('Bald Cypress', 600)]
-
-lifespan_lists = [animal_lifespans, tree_lifespans]
-
-for merged_item in heapq.merge(*lifespan_lists, key=lambda x: x[1]):
-    print(merged_item)
-
-import heapq
-
-
 class BSBIIndex(BSBIIndex):
     def merge(self, indices, merged_index):
-
-        ### Begin your code
-
-        ### End your code
-
-
-BSBI_instance = BSBIIndex(data_dir=toy_dir, output_dir = 'toy_output_dir', )
-BSBI_instance.index()
-
-BSBI_instance = BSBIIndex(data_dir='pa1-data', output_dir = 'output_dir', )
-BSBI_instance.index()
-
-
-BSBI_instance = BSBIIndex(data_dir='pa1-data', output_dir = 'output_dir', )
-BSBI_instance.intermediate_indices = ['index_'+str(i) for i in range(10)]
-with InvertedIndexWriter(BSBI_instance.index_name, directory=BSBI_instance.output_dir, postings_encoding=BSBI_instance.postings_encoding) as merged_index:
-    with contextlib.ExitStack() as stack:
-        indices = [stack.enter_context(InvertedIndexIterator(index_id, directory=BSBI_instance.output_dir, postings_encoding=BSBI_instance.postings_encoding)) for index_id in BSBI_instance.intermediate_indices]
-        BSBI_instance.merge(indices, merged_index)
+        term_id = -1
+        postings_list = []
+        for pair in heapq.merge(*indices, key=lambda x: x[0]):
+            if pair[0] != term_id:
+                if term_id != -1:
+                    merged_index.append(term_id, sorted(set(postings_list)))
+                term_id = pair[0]
+                postings_list = []
+            postings_list += pair[1]
+        merged_index.append(term_id, sorted(set(postings_list)))
 
 
 class InvertedIndexMapper(InvertedIndex):
@@ -383,50 +251,122 @@ class InvertedIndexMapper(InvertedIndex):
         return self._get_postings_list(key)
 
     def _get_postings_list(self, term):
-
-        ### Begin your code
-
-        ### End your code
-
+        pos, length, size = self.postings_dict[term]
+        self.index_file.seek(pos)
+        return self.postings_encoding.decode(self.index_file.read(size))
 
 def sorted_intersect(list1, list2):
 
-
-% % tee
-submission / retrieve.py
+    ret = []
+    i = 0
+    j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            i += 1
+        elif list2[j] < list1[i]:
+            j += 1
+        else:
+            ret.append(list1[i])
+            i += 1
+            j += 1
+    return ret
 
 
 class BSBIIndex(BSBIIndex):
     def retrieve(self, query):
-
         if len(self.term_id_map) == 0 or len(self.doc_id_map) == 0:
             self.load()
-
-        ### Begin your code
-
-        ### End your code
-
-
-
-BSBI_instance = BSBIIndex(data_dir='pa1-data', output_dir = 'output_dir', )
-BSBI_instance.retrieve('boolean retrieval')
-
-
-with open("pa1-data/1/cs276.stanford.edu_", 'r') as f:
-    print(f.read())
+        intersected = False
+        intersected_postings = []
+        with InvertedIndexMapper('BSBI', directory=self.output_dir) as index_mapper:
+            for token in query.split(' '):
+                if intersected:
+                    intersected_postings = sorted_intersect(index_mapper[self.term_id_map[token]], intersected_postings)
+                else:
+                    intersected_postings = index_mapper[self.term_id_map[token]]
+                    intersected = True
 
 
-for i in range(1, 9):
-    with open('dev_queries/query.' + str(i)) as q:
-        query = q.read()
-        my_results = [os.path.normpath(path) for path in BSBI_instance.retrieve(query)]
-        with open('dev_output/' + str(i) + '.out') as o:
-            reference_results = [os.path.normpath(x.strip()) for x in o.readlines()]
-            assert my_results == reference_results, "Results DO NOT match for query: "+query.strip()
-        print("Results match for query:", query.strip())
+class CompressedPostings:
+    def encode_int(gap):
+        ret = [(gap & 0x7f) | 0x80]
+        gap >>= 7
+        while gap != 0:
+            ret.insert(0, gap & 0x7f)
+            gap >>= 7
+        return ret
+    def encode(postings_list):
+        encoded_postings_list = []
+        encoded_postings_list += CompressedPostings.encode_int(postings_list[0])
+        for i in range(1, len(postings_list)):
+            encoded_postings_list += CompressedPostings.encode_int(postings_list[i] - postings_list[i - 1])
+        return array.array('B', encoded_postings_list).tobytes()
+    def decode(encoded_postings_list):
+        decoded_postings_list = array.array('B')
+        decoded_postings_list.frombytes(encoded_postings_list)
+
+        postings_list = []
+        base, n = 0, len(decoded_postings_list)
+        idx = 0
+        while idx < n:
+            gap = 0
+            while idx < n and (decoded_postings_list[idx] & 0x80) == 0:
+                gap = (gap << 7) | (decoded_postings_list[idx] & 0x7f)
+                idx += 1
+            gap = (gap << 7) | (decoded_postings_list[idx] & 0x7f)
+            idx += 1
+
+            posting = base + gap
+            postings_list.append(posting)
+            base = posting
+        return postings_list
 
 
-set(my_results) - set(reference_results)
+class ECCompressedPostings:
+    def encode_int(gap):
+        if gap == 0 or gap == 1:
+            return '0'
+        ret = '1' * int(log(gap, 2)) + '0' + bin(gap)[3:]
+        print(ret)
+        return ret
 
-set(reference_results) - set(my_results)
 
+    def encode(postings_list):
+        encoded_postings_list = ''
+        encoded_postings_list += ECCompressedPostings.encode_int(postings_list[0] - (-1))
+        for i in range(1, len(postings_list)):
+            encoded_postings_list += ECCompressedPostings.encode_int(postings_list[i] - postings_list[i - 1])
+        print(encoded_postings_list)
+        return array.array('B', [int(encoded_postings_list[x:x + 8], 2) for x in
+                                 range(0, len(encoded_postings_list), 8)]).tobytes()
+    def decode(encoded_postings_list):
+        decoded_bytes_list = array.array('B')
+        decoded_bytes_list.frombytes(encoded_postings_list)
+
+        decoded_postings_list = ''.join([bin(x)[2:].zfill(8) for x in decoded_bytes_list])
+        decoded_postings_list = decoded_postings_list[:-7] + bin(decoded_bytes_list[-1])[2:]
+        print(decoded_postings_list)
+
+        postings_list = []
+        base, idx, n = -1, 0, len(decoded_postings_list)
+        while idx < n:
+            length = 0
+            while idx < n and decoded_postings_list[idx] == '1':
+                length += 1
+                idx += 1
+            if idx < n:
+                # '111...1(length)0xxx...x(length)', length maybe 0
+                idx = idx + 1 + length
+                gap = int('1' + decoded_postings_list[idx - length: idx], 2)
+                print(idx, gap)
+                posting = base + gap
+                postings_list.append(posting)
+                base = posting
+        return postings_list
+try:
+    os.mkdir('output_dir_compressed')
+except FileExistsError:
+    pass
+
+BSBI_instance_compressed = BSBIIndex(data_dir='pa1-data', output_dir = 'output_dir_compressed', postings_encoding=CompressedPostings)
+BSBI_instance_compressed.retrieve('boolean retrieval')
