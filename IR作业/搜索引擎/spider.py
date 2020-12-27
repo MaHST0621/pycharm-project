f40e58f8debb7eef7f3ac822678896967904d90a
MaHST0621 <1186217640@qq.com>
2020-12-28 00:39:28 +0800

    基本东西能爬取了

--- IR作业/搜索引擎/spider.py
+++ IR作业/搜索引擎/spider.py
@@ -10,6 +10,7 @@ driver = webdriver.Chrome()
 def s(int):
     time.sleep(int)
 
+
 class URL_TOOL:
     def __init__(self,continue_url = '',):
         if continue_url == '':
@@ -21,29 +22,47 @@ class URL_TOOL:
         self.ContinueUrl = driver.find_elements_by_xpath("//a[@href]")
         f = open("docs.txt", "w")
         for elem in self.ContinueUrl:
-            f.write(elem.get_attribute("href") + '\n')
+             f.write(elem.get_attribute("href") + '\n')
+        f.close()
+        f = open("docs.txt","r")
+        file = open("test.txt", "w")
+        for lin in f.readlines():
+            if "cc.nankai" in lin :
+                file.write(str(lin) + '\n')
 
     def get_html_title(self,file):
         return (file[7:-8])
+
+    def remove(self,string):
+        string = string.replace(" ","")
+        string = string.replace("/","_")
+        string = string.replace("【","")
+        string = string.replace("】", "")
+        string = string.replace("“", "")
+        string = string.replace("”", "")
+        string = string.replace("\"", "")
+        return string
+
     def get_UrlPage(self):
-        f = open("docs.txt")
+        f = open("test.txt")
         lines = f.readlines()
         for url1 in lines:
-            if(url1[0] == "j"):
-                continue
             try:
                 driver.get(url1)
             except:
                 continue
             html = driver.find_element_by_xpath("//*").get_attribute("outerHTML")
             soup = BeautifulSoup(html, 'lxml')
-            self.get_html_title(str(soup.title))
-            srtt = str(soup.title).replace('/','_')
-            file = open("html_file/" + self.get_html_title(srtt) + '.txt',"w")
-            file
+            srtt = self.remove(str(soup.title))
+            file = open("html_file/" + self.get_html_title(srtt) + '.txt',"a+")
+            try:
+                text = driver.find_element_by_class_name('wp_articlecontent').text
+                file.write(text)
+            except:
+                continue
 
 if __name__ == '__main__':
     spider = URL_TOOL()
-    # spider.get_Continue_Url(Nankai_cs_url)
+    pider.get_Continue_Url(Nankai_cs_url)
     spider.get_UrlPage()
 
