84b153c3993d8e6e9a89bc2d0c9088a6a947fc6a
MaHST0621 <1186217640@qq.com>
2021-01-02 17:01:47 +0800

    pagerank finish

--- IR作业/搜索引擎/pagerank.py
+++ IR作业/搜索引擎/pagerank.py
@@ -1,10 +1,50 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import bloompy
 import networkx as nx
+import matplotlib.pyplot as plt
+import pickle as pkl
 # 创建有向图
 G = nx.DiGraph()
-# 有向图之间边的关系
-edges = [("A", "B"), ("A", "C"), ("A", "D"), ("B", "A"), ("B", "D"), ("C", "A"), ("D", "B"), ("D", "C")]
+bloom_url = bloompy.ScalableBloomFilter(error_rate=0.001,initial_capacity=10**5)
+driver = webdriver.Chrome()
+Nankai_cs_url = 'http://cc.nankai.edu.cn'
+edges = {}
+i = 0
+def get_Continue_Url(url):
+    if bloom_url.add(url) == True:
+        return
+    try:
+        driver.get(url)
+        continue_link = driver.find_element_by_tag_name('a')
+        ContinueUrl = driver.find_elements_by_xpath("//a[@href]")
+    except:
+        return -1
+    for elem in ContinueUrl:
+        if elem.get_attribute("href") not in edges.setdefault(url,[]):
+            if "cc.nankai" in elem.get_attribute("href"):
+                if "articleId" not in elem.get_attribute("href"):
+                    edges[url].append(elem.get_attribute("href"))
+    for elem in edges[url]:
+        get_Continue_Url(elem)
+def get_Utrl_edge():
+    result = []
+    for i in range(0,len(edges)):
+        for j in edges.keys():
+            for url in edges[j]:
+                result.append((j,url))
+    f = open("pagerank.list","wb+")
+    pkl.dump(result,f)
+    f.close()
+    return result
 
-for edge in edges:
-    G.add_edge(edge[0], edge[1])
-pagerank_list = nx.pagerank(G, alpha=1)
-print("pagerank值是：\n", pagerank_list)
+if __name__ == '__main__':
+    f = open("pagerank.list","rb+")
+    results = pkl.load(f)
+    print("test1")
+    for edge in results:
+        G.add_edge(edge[0], edge[1])
+    pagerank_list = nx.pagerank(G, alpha=0.85)
+    print(pagerank_list)
+    # nx.draw(G, pos=nx.random_layout(G),node_color = 'b', edge_color = 'r', with_labels = False,font_size = 18, node_size = 20)
+    # plt.show()
